0e47d90154261f3fb4574b7ac2dc07cc7edecaf0
CHILA <ivanalvatec115@gmail.com>
2026-04-16 12:33:05 -0600

    Update supabase_client.py

--- supabase_client.py
+++ supabase_client.py
@@ -1,14 +1,8 @@
 import streamlit as st
 from supabase import create_client, Client
-from supabase.lib.client_options import ClientOptions
 
 @st.cache_resource(show_spinner=False)
 def get_supabase() -> Client:
     url = st.secrets["SUPABASE_URL"]
     key = st.secrets["SUPABASE_KEY"]
-
-    options = ClientOptions(
-        postgrest_client_timeout=10,
-        storage_client_timeout=10,
-    )
-    return create_client(url, key, options=options)
+    return create_client(url, key)
